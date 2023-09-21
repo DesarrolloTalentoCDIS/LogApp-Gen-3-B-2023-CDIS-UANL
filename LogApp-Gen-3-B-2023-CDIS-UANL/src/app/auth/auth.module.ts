7fc6543282aef0c52278c40970bc73d10544a039
MoisesDavila000 <m.ivan.dr.10@gmail.com>
2023-09-21 13:26:31 -0600

    11 Generar componentes para auth

--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/auth.module.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    MainComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
