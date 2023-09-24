032f090691d3f33e0046fb902858e855476efa2c
hvanjerdz <hvanjerdz@proton.me>
2023-09-24 13:04:12 -0600

    20 Configura el Service

--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/services/auth.service.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { user } from '../interfaces/us-interface'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,25 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _user! : user;
 
-  constructor() { }
+  get user()  {
+    return {...this._user}
+  }
+
+  constructor(private http: HttpClient) { }
+
+  register(usname : string, id : string, pass : string) {
+    const URL = `${this.baseUrl}/auth/new`;
+    const body = {usname, id, pass};
+  }
+
+  login(id: string, pass: string) {
+    const URL = `${this.baseUrl}/auth/`;
+    const body = {id, pass};
+  }
+
+  validateToken() {
+    
+  }
 }
