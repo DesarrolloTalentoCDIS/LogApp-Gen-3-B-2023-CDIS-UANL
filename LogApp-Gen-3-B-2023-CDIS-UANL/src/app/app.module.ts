652d03af5ac8fc6db3045241bc32417adec01c2d
MoisesDavila000 <m.ivan.dr.10@gmail.com>
2023-09-27 13:01:05 -0600

    23 Añadir notificaciones
    Añade la librería y configura las notifaiciones de la app para un mensaje basico.  V6

--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/app.module.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { HttpClientModule } from '@angular/common/http'
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
-  
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/components/login/login.component.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/auth/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -17,11 +18,21 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private toastr: ToastrService) { }
 
   login(){
-    console.log(this.formularioLog.value);
-    console.log(this.formularioLog.valid);
-    this.router.navigateByUrl('/dashboard');
+    // console.log(this.formularioLog.value);
+    // console.log(this.formularioLog.valid);
+    // this.router.navigateByUrl('/dashboard');
+
+    if(this.formularioLog.valid){
+      const{id,pass} = this.formularioLog.value;
+      this.router.navigateByUrl('/dashboard');
+
+    }
+    else{
+      this.toastr.error('Verifique sus datos', 'Error');
+    }
   }
 }
