1898043a1d6bac2a1ba2617568d385fa67cd51b1
Daniel Treviño <danieltrev09@gmail.com>
2023-10-01 18:17:32 -0600

    Configuracion de Service en base al guard
    basicamente hice las conexiones de la validacion de token que viene siendo la contraseña con el guard que evita que iniciemos sin claves correctas y nos regresaria al inicio de cuenta una vez que configuren las rutas que seria el proximo punto a realizar por mi compañero.

--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/guards/validate-token.guard.spec.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/guards/validate-token.guard.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
-
 import { validateTokenGuard } from './validate-token.guard';
 
 describe('validateTokenGuard', () => {
--- LogApp-Gen-3-B-2023-CDIS-UANL/src/app/guards/validate-token.guard.ts
+++ LogApp-Gen-3-B-2023-CDIS-UANL/src/app/guards/validate-token.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 import { AuthService } from '../auth/services/auth.service';
 
 @Injectable({
 providedIn: 'root'
 })
-export class validateTokenGuard implements CanActivate, CanLoad {
+export class ValidateTokenGuard implements CanActivate, CanLoad {
 
-  constructor(private authService : AuthService)
-  private router: Router) { }
+  constructor(private authService : AuthService,
+  private router : Router) { }
 
   canActivate(): Observable<boolean> | boolean {
     return this.authService.validateToken()
@@ -24,6 +24,15 @@ export class validateTokenGuard implements CanActivate, CanLoad {
 
 
   canLoad(): Observable<boolean> | boolean {
-    return this.authService.validateToken
+    return this.authService.validateToken()
+    .pipe(
+      tap(valid => {
+        if(!valid){
+          this.router.navigateByUrl('/auth');
+        }
+      })
+    );
   }
-};
+}
+
+
